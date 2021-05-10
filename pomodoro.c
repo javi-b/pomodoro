e5b29307ac18ca721b5205987c420617ac4da983
javi-b <job1001@alu.ubu.es>
2021-05-10 15:19:59 +0200

    dirty implementation of notifications for linux

--- pomodoro.c
+++ pomodoro.c
@@ -1,6 +1,8 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
 
 #define CICLE 4
 #define WORK_TIME 25
@@ -9,11 +11,21 @@
 
 enum period{work, short_break, long_break} period;
 const int PERIOD_TIMES[3] = {WORK_TIME, SHORT_BREAK_TIME, LONG_BREAK_TIME};
-const char PERIOD_MESSAGES[3][140] = {
+const char PERIOD_MESSAGES[3][128] = {
     "Time to work, you can do this!",
     "Enjoy a short break :)",
     "You have a longer break now!"
 };
+
+void notify() {
+    printf("%s\n", PERIOD_MESSAGES[period]);
+
+    char command[128];
+    strcpy(command, "notify-send \"");
+    strcat(command, PERIOD_MESSAGES[period]);
+    strcat(command, "\"");
+    system(command);
+}
 
 int main(int argc, char *argv[]) {
     time_t start_time, current_time;
@@ -22,7 +34,7 @@ int main(int argc, char *argv[]) {
     int step = 1;
     period = work;
     start_time = time(NULL);
-    printf("%s\n", PERIOD_MESSAGES[period]);
+    notify();
 
     while(1) {
         current_time = time(NULL);
@@ -40,7 +52,7 @@ int main(int argc, char *argv[]) {
                 period = short_break;
             }
 
-            printf("%s\n", PERIOD_MESSAGES[period]);
+            notify();
         }
 
         sleep(60); // sleeps 1 minute
